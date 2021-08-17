b753a1024d13e6f299aa52f70f61779ebdec9dda
Gio Lodi <giovanni.lodi42@gmail.com>
2021-08-17 16:29:21 +1000

    Add docs for waitForHittability

--- Sources/XCUITestHelpers/XCUIElement+Async.swift
+++ Sources/XCUITestHelpers/XCUIElement+Async.swift
@@ -2,10 +2,16 @@ import XCTest
 
 extension XCUIElement {
 
-    /// Beware that this uses `XCTNSPredicateExpectation` which relies on polling and is slower than
-    /// other asynchronous expectations.
+    /// Waits for `self` to be hittable (for `isHittable` to be `true`) for the given `timeout`,
+    /// returning `true` if the wait was successful and `false` otherwise
+    ///
+    /// Beware that this uses `XCTNSPredicateExpectation` which relies on polling and is slower
+    /// that other asynchronous expectations.
     @discardableResult
     public func waitForIsHittable(timeout: TimeInterval = 3.0) -> Bool {
+        // Avoid unnecessary waiting if the element is already hittable
+        guard isHittable == false else { return true }
+
         let result = XCTWaiter.wait(
             for: [
                 XCTNSPredicateExpectation(
