5977fc1d117e2fd33dfdece6758360c3c9407d73
Gio Lodi <giovanni.lodi42@gmail.com>
2021-08-18 16:28:48 +1000

    Restructure `ContentView` to make it easier to follow (hopefully)

--- PlaygroundApp/ContentView.swift
+++ PlaygroundApp/ContentView.swift
@@ -1,56 +1,72 @@
 import SwiftUI
 
-struct DummyListItem: Identifiable {
-
-    let text: String
-
-    var id: String { text }
-
-    static let dummyValues: [DummyListItem] = "abcdefghijklmnopqrstuvwxyz"
-        .map { char in
-            DummyListItem(text: String(char))
-        }
-}
-
-struct MonospacedText: View {
-
-    let text: String
-
-    init(_ text: String) {
-        self.text = text
-    }
+struct ContentView: View {
 
     var body: some View {
-        Text(text).font(.system(.body, design: .monospaced))
+        VStack(spacing: 20) {
+            StatefulTextWithButton()
+            Matrix()
+            DummyList()
+        }
     }
 }
 
-struct ContentView: View {
+/// A view consisting of a text label and a button. When the button is tapped, the text in the
+/// label changes.
+struct StatefulTextWithButton: View {
+
     @State var text = "button has not been tapped"
 
     var body: some View {
         VStack {
-            Text(text).padding()
+            Text(text)
             Button("button") {
                 text = "button was tapped"
             }
-            .padding()
+        }
+    }
+}
 
-            // Using monospace font to ensure constant relative positioning.
-            HStack {
-                MonospacedText("x1-y1")
-                MonospacedText("x2-y1")
-                MonospacedText("x3-y1")
-            }
-            HStack {
-                MonospacedText("x1-y2")
-                MonospacedText("x2-y2")
-                MonospacedText("x3-y2")
-            }
+/// A view with a matrix of text subviews. Use it to test methods that compute how views are relative to
+/// each other
+struct Matrix: View {
 
-            List(DummyListItem.dummyValues) { item in
-                Text(item.text)
+    var body: some View {
+        VStack {
+            HStack {
+                Text("x1-y1")
+                Text("x2-y1")
+                Text("x3-y1")
             }
+            HStack {
+                Text("x1-y2")
+                Text("x2-y2")
+                Text("x3-y2")
+            }
+        }
+        // Using monospace font to ensure constant relative positioning.
+        .font(.system(.body, design: .monospaced))
+    }
+}
+
+/// A `List` showing dummy items. Use it to test basic list interaction behaviors, like scrolling
+/// and finding cells.
+struct DummyList: View {
+
+    struct Item: Identifiable {
+
+        let text: String
+
+        var id: String { text }
+    }
+
+    /// An array of `DummyList.Item`s with each element representing a letter of the English
+    /// alphabet.
+    let values: [Item] = "abcdefghijklmnopqrstuvwxyz".map { Item(text: "\($0)") }
+
+    var body: some View {
+        List(values) { item in
+            Text(item.text)
         }
     }
 }
