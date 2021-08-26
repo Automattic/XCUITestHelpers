d6988ac789aec294a97bd917b9a36f8b2f2fe431
Gio Lodi <giovanni.lodi42@gmail.com>
2021-08-26 12:55:20 +1000

    Add comment regarding hardcoded iOS version in tests command
    Thanks for the suggestion @AliSoftware.
    
    See
    https://github.com/Automattic/XCUITestHelpers/pull/4#discussion_r695601359

--- .buildkite/commands/run-tests.sh
+++ .buildkite/commands/run-tests.sh
@@ -1,5 +1,18 @@
 #!/bin/bash -eu
 
+# Notice that:
+#
+# - We are using raw `xcodebuild` without extra tooling such as Fastlane,
+#   `xcpretty`, or `xcbeautify`. This is an intentional compromise to keep the
+#   setup lean, as adding any of those would require extras in CI such as
+#   chating for what, right now, looks like merely a log readability gain.
+#
+# - The iOS Simulator version is hardcoded in the command call, unlike the
+#   Simulator device, so we have a single source of truth. The downside is that
+#   the syntax in the pipeline is less clear: a reader might as where's the
+#   iOS version set. As long as we only need to test against the latest iOS
+#   version, that seems like a reasonable tradeoff to make it easier to move to
+#   newer versions as they are released.
 SIMULATOR_NAME=$1
 
 xcodebuild clean test \
