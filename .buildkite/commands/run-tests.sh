0839007cc124879f595ff01280ab93335dd0eb14
Gio Lodi <giovanni.lodi42@gmail.com>
2021-09-06 14:23:06 +1000

    Extract Buildkite simulator version in script constant + reword comment
    Hat tip @AliSoftware.
    
    See
    https://github.com/Automattic/XCUITestHelpers/pull/4#discussion_r696399590

--- .buildkite/commands/run-tests.sh
+++ .buildkite/commands/run-tests.sh
@@ -4,18 +4,19 @@
 #
 # - We are using raw `xcodebuild` without extra tooling such as Fastlane,
 #   `xcpretty`, or `xcbeautify`. This is an intentional compromise to keep the
-#   setup lean, as adding any of those would require extras in CI such as
-#   chating for what, right now, looks like merely a log readability gain.
+#   setup lean, as adding any of those would require extra tooling and
+#   ceremony in CI for what, right now, is merely a gain in log readability.
 #
-# - The iOS Simulator version is hardcoded in the command call, unlike the
-#   Simulator device, so we have a single source of truth. The downside is that
-#   the syntax in the pipeline is less clear: a reader might as where's the
-#   iOS version set. As long as we only need to test against the latest iOS
+# - The iOS Simulator version is hardcoded in the script, unlike the Simulator
+#   device, so we have a single source of truth. The downside is that the
+#   syntax in the pipeline is less clear: a reader might ask where the iOS
+#   version is set. As long as we only need to test against the latest iOS
 #   version, that seems like a reasonable tradeoff to make it easier to move to
 #   newer versions as they are released.
 SIMULATOR_NAME=$1
+SIMULATOR_VERSION='14.5'
 
 xcodebuild clean test \
   -project 'XCUITestHelpers.xcodeproj' \
   -scheme 'XCUITestHelpers' \
-  -destination "platform=iOS Simulator,name=$SIMULATOR_NAME,OS=14.5"
+  -destination "platform=iOS Simulator,name=$SIMULATOR_NAME,OS=$SIMULATOR_VERSION"
