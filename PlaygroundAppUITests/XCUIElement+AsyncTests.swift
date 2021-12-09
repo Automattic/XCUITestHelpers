da6ae1eb8fdf8bae51bc6c1cff5c17fec25b0ba4
Gio Lodi <giovanni.lodi42@gmail.com>
2021-12-09 11:42:27 +0100

    Add `XCUIElement` helper methods to wait for an `NSPredicate`

--- PlaygroundAppUITests/XCUIElement+AsyncTests.swift
+++ PlaygroundAppUITests/XCUIElement+AsyncTests.swift
@@ -1,8 +1,42 @@
-//
-//  XCUIElement+AsyncTests.swift
-//  PlaygroundAppUITests
-//
-//  Created by Gio on 9/12/21.
-//
+import XCUITestHelpers
+import XCTest
 
-import Foundation
+class XCUIElementAsyncTests: XCTestCase {
+
+    let app = XCUIApplication()
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app.launch()
+    }
+
+    func testWaitsForPredicateOnElement() {
+        XCTAssertEqual(
+            app.staticTexts["a"].waitFor(
+                predicate: NSPredicate(format: "isEnabled == true"),
+                timeout: 2 // Interestingly, on Xcode 13.1, a 1 second timeout results in a failure
+            ),
+            .completed
+        )
+        // Test the opposite scenario must also be true.
+        XCTAssertEqual(
+            app.staticTexts["a"].waitFor(
+                predicate: NSPredicate(format: "isEnabled == false"),
+                timeout: 2
+            ),
+            .timedOut
+        )
+    }
+
+    func testWaitsForPredicateFromStringOnElement() {
+        XCTAssertEqual(
+            app.staticTexts["a"].waitFor(predicateString: "isEnabled == true"),
+            .completed
+        )
+        // Test the opposite scenario must also be true.
+        XCTAssertEqual(
+            app.staticTexts["a"].waitFor(predicateString: "isEnabled == false"),
+            .timedOut
+        )
+    }
+}
