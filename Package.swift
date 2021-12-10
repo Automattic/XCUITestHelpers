b0eb69cabb6d2044549c3dfbf820d2418d9c9221
Josh Heald <joshheald@users.noreply.github.com>
2021-12-10 11:17:21 +0000

    add mac catalyst support

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "XCUITestHelpers",
-    platforms: [ .iOS(.v13) ],
+    platforms: [ .iOS(.v13), .macOS(.v10_13) ],
     products: [
         .library(name: "XCUITestHelpers", targets: ["XCUITestHelpers"]),
     ],
